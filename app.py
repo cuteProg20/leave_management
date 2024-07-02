02cb0cc75c3475d3d7c4c09168195fbab208dd67
cuteProg <janeyjackytz@gmail.com>
2024-07-02 10:42:43 +0300

    hkjh

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from flask_migrate import Migrate
 from apps import db, create_app
 import os
 
-<<<<<<< HEAD
-=======
-app = create_app()
-migrate = Migrate(app, db)
->>>>>>> 6be26e20d84ce904fc952c8113db523893adaad1
 
 app= Flask(__name__)
 
@@ -20,15 +15,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def create_tables():
     db.create_all()
 
-<<<<<<< HEAD
 # @app.route('/home')   
 # def home():
 #     return render_template('apps/Templates/home.html')
-=======
-@app.route('/home')   
-def home():
-    return render_template('apps/Templates/home.html')
->>>>>>> 6be26e20d84ce904fc952c8113db523893adaad1
 
 
 if __name__ == '__main__':
